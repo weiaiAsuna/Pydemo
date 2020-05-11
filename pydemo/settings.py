be1f184519237024245a6a7b7f1ed8d3b7b6af5b
weiaiAsuna <1912296101@qq.com>
2020-05-11 21:45:04 +0800

    默认更改列表

--- pydemo/settings.py
+++ pydemo/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,17 +35,55 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "demo1","app01", "second_app",'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    # 'second_app.middleware.crossdomainxhr.XsSharing',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+# django后端解决跨域方式二django-cors-headers
+#跨域增加忽略
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = ()
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
+
+# django后端解决跨域方式一Middleware
+# crossdomain
+#XS_SHARING_ALLOWED_ORIGINS ='*'
+# XS_SHARING_ALLOWED_ORIGINS = 'http://127.0.0.1:8000'
+# XS_SHARING_ALLOWED_METHODS = ['POST', 'GET']
+# XS_SHARING_ALLOWED_HEADERS = ['Content-Type', '*']
+# XS_SHARING_ALLOWED_CREDENTIALS = 'true'
 
 ROOT_URLCONF = 'pydemo.urls'
 
@@ -69,17 +105,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pydemo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# 连接MySQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nodetest',#数据库名
+        'USER': 'root',#用户名
+        'PASSWORD': '123456',#密码
+        'HOST': '127.0.0.1',#ip
+        'PORT': '3306',#端口号
+        'DEFAULT_CHARSET':'utf-8'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -99,12 +146,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
-#语言英文en-us   中文zh-hans
+# 语言英文en-us   中文zh-hans
 LANGUAGE_CODE = 'zh-hans'
-#时区  标准时间UTC 中国用上海的Asia/Shanghai
+# 时区  标准时间UTC 中国用上海的Asia/Shanghai
 TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
@@ -113,8 +159,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    ('css',os.path.join(STATIC_ROOT, 'css')),
+    ('js',os.path.join(STATIC_ROOT, 'js')),
+    ("upload",os.path.join(BASE_DIR,'upload'))
+)
--- pydemo/urls.py
+++ pydemo/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^second/',include("second_app.urls")),
+    url(r'^demo1/',include("demo1.urls")),
+    url(r'^app01/',include("app01.urls")),
 ]
